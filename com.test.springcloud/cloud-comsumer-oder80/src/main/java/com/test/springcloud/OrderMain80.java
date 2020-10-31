f776948fd248b2e72e81586c4437cbd242994837
26324 <2632491776@qq.com>
2020-11-01 01:57:46 +0800

    springcloud eureka

---
+++ com.test.springcloud/cloud-comsumer-oder80/src/main/java/com/test/springcloud/OrderMain80.java
@@ -0,0 +1,14 @@
+package com.test.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableEurekaClient
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class, args);
+    }
+}
