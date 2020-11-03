1f6b84616cd2626ab9186ec1894f4f66feff87bf
26324 <2632491776@qq.com>
2020-11-04 00:55:01 +0800

    springcloud zookeeper 完成

---
+++ com.test.springcloud/cloud-consumerzk-order80/src/main/java/springcloud/controller/OrderZKController.java
@@ -0,0 +1,23 @@
+package springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@Slf4j
+@RestController
+public class OrderZKController {
+//    private final static String PAYMENT_URL = "http://localhost:8001";
+    private final static String PAYMENT_URL = "http://cloud-provider-payment";
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @GetMapping("/consumer/payment/zk")
+    public String paymentInfo() {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/zk/", String.class);
+    }
+}
